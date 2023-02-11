e7ad808cb5334dc9283188ec8a7fdcc302b2687c
Pavel Kholyavin <p.kholyavin@mail.ru>
2023-02-11 06:56:47 +0300

    cleaned old files

--- caesar.py
+++
@@ -1,6 +0,0 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-message = "ittgwczjiamizmjmtwvobwca"
-
-for shift in range(1, 26):
-    print(shift, "".join([alphabet[alphabet.index(letter) - shift] for letter in message]))
--- print_nums.py
+++
@@ -1,5 +0,0 @@
-def f1():
-    print("This is f1")
-
-def f2():
-    print("This is f2")
